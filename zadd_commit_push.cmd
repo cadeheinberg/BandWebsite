757ddb983a588ccc56d050e866d7414f4164382b
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2022-06-26 18:25:35 -0500

    test

--- zadd_commit_push.cmd
+++ zadd_commit_push.cmd
@@ -1,4 +1,4 @@
 git add -A
 git commit -m "test"
 git push
-ssh -i C:\\Users\\cadeh\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "bash /var/www/mysites/brockhampton-website/pull_from_github.sh ; bash --login"
+ssh -i C:\\Users\\Cade\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "bash /var/www/mysites/brockhampton-website/pull_from_github.sh ; bash --login"
