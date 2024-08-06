1b968c08970ad161e5c55545cb1d5ec4c79214c6
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2024-08-05 23:49:50 -0500

    Delete zadd_commit_push.cmd

--- zadd_commit_push.cmd
+++
@@ -1,4 +0,0 @@
-git add -A
-git commit -m "test"
-git push
-ssh -i C:\\Users\\Cade\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "bash /var/www/mysites/brockhampton-website/pull_from_github.sh ; bash --login"
